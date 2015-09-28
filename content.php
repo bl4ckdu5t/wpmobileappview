347e424ed5227a5365cd116d2299b69c09fa1583
bl4ckdu5t <joerexko@gmail.com>
2015-09-28 07:43:44 -0500

    Added content.php to try to fetch contents

---
+++ content.php
@@ -0,0 +1,8 @@
+<li>
+  <span class="post__tag">Entertainment</span>
+  <img src="http://placehold.it/320x250" alt="" class="post__thumb">
+  <article class="post__excerpts__article" id="post-<?php the_ID(); ?>">
+    <h2><a href="">The revival of lulzSec</a></h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, repellat.</p>
+  </article>
+</li>

3de688417690f7cd63c72bd82cd2cd877c116ce4
bl4ckdu5t <joerexko@gmail.com>
2015-09-28 06:00:00 -0500

    modded functions.php for enqueue style

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function theme_name_scripts() {
+	wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
